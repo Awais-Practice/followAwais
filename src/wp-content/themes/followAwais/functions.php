1f6c913f16b2916cc7d95804ea35aba6c29f4f8e
awaismalik <awais.malik.q@gmail.com>
2021-02-28 22:18:05 +0500

    In this section of code style links(In header) and script link(In footer) comes form functions file dinamico way

---
+++ src/wp-content/themes/followAwais/functions.php
@@ -0,0 +1,32 @@
+<?php
+function followawais_register_styles(){
+    $version =wp_get_theme()-> get('Version');
+    wp_enqueue_style('followawais-style', get_template_directory_uri()."/style.css",array(), $version,'all');
+
+    wp_enqueue_style('followawais-font-awesome',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(),'5.13.0','all');
+    wp_enqueue_style('followawais-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",array(),'4.4.1','all');
+}
+
+
+add_action('wp_enqueue_scripts','followawais_register_styles');
+function followawais_register_scripts(){
+
+    wp_enqueue_script('followawais-jquery',"https://code.jquery.com/jquery-3.4.1.slim.min.js",array(),'3.4.1'); 
+     wp_enqueue_script('followawais-popper',"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",array(),'1.16.0');  
+
+    wp_enqueue_script('followawais-bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js",array(),'4.4.1');  
+
+  wp_enqueue_script('followawais-main', get_template_directory_uri()."assets/js/main.js",array(), '1.0','all');
+   
+}
+add_action('wp_enqueue_scripts','followawais_register_scripts');
+
+
+
+
+
+
+
+  
+
+?>
